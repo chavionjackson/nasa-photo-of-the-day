f39dace3f9bfc66aeb23ea3b714f6d982a09b11a
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-04-28 13:32:47 -0700

    Styled buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -5,9 +5,11 @@ const Button = styled.button`
   padding: 6px 10px;
   margin: 2rem;
   border: none;
-  border-radius: 3px;
+  border-radius: 9px;
   color: white;
   background-color: #7649fe;
+  font-size: 2vh;
+  font-weight: bolder;
 `;
 
 export default Button;
